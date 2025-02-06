076461047eee5f2bab9747c590b9ef05b5472c02
BG1-VN <jessica.reis@vn2r.pt>
2025-02-06 15:23:15 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ miro.onReady(() => {
     // When the button is clicked, open the Streamlit app in a modal
     document.getElementById("open-streamlit").addEventListener("click", () => {
         miro.board.ui.openModal({
-            url: "https://mirotommd.streamlit.app/",  // Replace with your Streamlit app URL
+            url: "https://miro-board-exporter-teste.streamlit.app/",  // Your Streamlit app URL
             width: 800,  // Adjust modal width
             height: 600, // Adjust modal height
         });
